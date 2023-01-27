6b4f17a69594dd84ed57f4e049fc6958a7c8433b
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-27 10:21:07 -0500

    Update checkValidators.ts

--- src/middlewares/validators/checkValidators.ts
+++ src/middlewares/validators/checkValidators.ts
@@ -6,8 +6,8 @@ import { validationResult } from 'express-validator'
 export const checkValidations = (req: Request, _res: Response, next: NextFunction): any => {
   const errors = validationResult(req)
 
+  // [{ ..., msg }] -> [msg, msg, ...] -> 'msg. msg. msg. msg'
   if (!errors.isEmpty()) {
-    // [{ ..., msg }] -> [msg, msg, ...] -> 'msg. msg. msg. msg'
     const errorMessages = errors.array().map((err) => err.msg)
     const message = errorMessages.join('. ')
     return next(new AppError(message, 400))
