0391eb4214da5032da612817a3eb67cd5081b0ac
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-27 10:21:52 -0500

    Update appRoutes.ts

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import { calendarRoutes } from './calendar'
 import { dataRoutes } from './data'
+import { journeyRoutes } from './journey'
+import { teamRoutes } from './team'
 import { userRoutes } from './user'
 
 export const appRoutes = Router()
 
 // rutas de acceso
-appRoutes.use('/users', userRoutes)
 appRoutes.use('/data', dataRoutes)
+appRoutes.use('/calendars', calendarRoutes)
+appRoutes.use('/journeys', journeyRoutes)
+appRoutes.use('/teams', teamRoutes)
+appRoutes.use('/users', userRoutes)
