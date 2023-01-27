f5e7dac58e50ea66b0ce11dcbd515139d6cbe20e
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-27 10:21:28 -0500

    add middelware validators team

---
+++ src/middlewares/validators/team.ts
@@ -0,0 +1,20 @@
+import { body } from 'express-validator'
+
+import { checkValidations } from './checkValidators'
+
+export const createTeamValidators = [
+  body('name').isLength({ min: 3, max: 50 }).withMessage('El nombre del equipo debe tener entre 3 y 50 caracteres'),
+  body('serie').isIn(['A', 'B', 'C']).withMessage('La serie debe ser A, B o C'),
+  body('season').isInt().withMessage('La temporada debe ser el año que se realiza el evento como número entero'),
+  checkValidations,
+]
+
+export const updateTeamValidators = [
+  body('name')
+    .optional()
+    .isLength({ min: 3, max: 50 })
+    .withMessage('El nombre del equipo debe tener entre 3 y 50 caracteres'),
+  body('series').optional().isIn(['A', 'B', 'C']).withMessage('La serie debe ser A, B o C'),
+  body('season').optional().isInt().withMessage('La temporada debe ser un número entero'),
+  checkValidations,
+]
