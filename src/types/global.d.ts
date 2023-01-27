9e176548ee833cf791efafdc515ca6d345cb5a5e
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-27 10:23:37 -0500

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,13 @@
-import { User } from '../models'
+import { Calendar, Journey, Team, User } from '../models'
 
 // global express interface
 declare global {
   namespace Express {
     export interface Request {
       user: User
+      team: Team
+      journey: Journey
+      calendar: Calendar
     }
   }
 }
