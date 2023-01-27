79ffa29eb8b6998ecfa0d54b569afc9b88bd4397
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-27 10:19:46 -0500

    add controller jurney

---
+++ src/controllers/journey.ts
@@ -0,0 +1,54 @@
+import type { NextFunction, Request, Response } from 'express'
+import type { IJourney } from '../models'
+
+import { journeyServices } from '../services'
+import { catchAsync } from '../utils'
+
+export const getAllJourneys = catchAsync(async (_req: Request, res: Response, _next: NextFunction) => {
+  const data = await journeyServices.getAll()
+
+  res.status(200).json({
+    status: 'success',
+    data,
+  })
+})
+
+export const getJourneyById = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+  const { journey } = req
+  res.status(200).json({
+    status: 'success',
+    data: journey,
+  })
+})
+
+export const createJourney = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+  const journey: IJourney = req.body
+  const data = await journeyServices.create(journey)
+
+  res.status(201).json({
+    status: 'success',
+    data,
+  })
+})
+
+export const updateJourneyById = catchAsync(async (req: Request, res: Response, _next: NextFunction) => {
+  const journeyUpdate: IJourney = req.body
+  const { journey } = req
+  const data = await journeyServices.update(journey, journeyUpdate)
+  res.status(200).json({
+    status: 'success',
+    data,
+  })
+})
+
+export const deleteJourney = catchAsync(async (req, res, next) => {
+  const { journey } = req
+
+  const isRemoved = await journeyServices.remove(journey)
+
+  if (isRemoved) {
+    res.status(200).json({ status: 'success' })
+  } else {
+    next(new Error('Failed to remove journey'))
+  }
+})
