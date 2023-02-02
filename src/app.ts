692f7fd2f01cf10974a26df5d55704007e09687f
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-01 21:48:18 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
+import cors from 'cors'
 import express from 'express'
 import morgan from 'morgan'
-import { appRoutes } from './routes'
 import { globalErrorHandler } from './controllers'
+import { appRoutes } from './routes'
 
 // Init our Express app
 export const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api/v1', appRoutes)
