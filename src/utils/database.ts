a1a75b129b2e03aba60392173a5d0de95a653376
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-21 14:59:59 -0500

    Update database.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,9 +1,41 @@
 import { Sequelize } from 'sequelize-typescript'
-import { Calendar, Image, Journey, Manager, Mvp, Player, PlayerStats, Team, TeamStats, User } from '../models'
+import {
+  Calendar,
+  Image,
+  Journey,
+  Manager,
+  Mvp,
+  Player,
+  PlayerStats,
+  Prediction,
+  Result,
+  Team,
+  TeamStats,
+  Transaction,
+  User,
+  Wallet,
+} from '../models'
+import { Ticket } from '../models/Ticket'
 import { config } from './config'
 
 export const db = new Sequelize({
   ...config.PROD,
   dialect: 'postgres',
-  models: [Calendar, Image, Journey, Manager, Mvp, Player, PlayerStats, Team, TeamStats, User],
+  models: [
+    Calendar,
+    Image,
+    Journey,
+    Manager,
+    Mvp,
+    Player,
+    PlayerStats,
+    Prediction,
+    Team,
+    TeamStats,
+    Ticket,
+    Transaction,
+    User,
+    Wallet,
+    Result,
+  ],
 })
